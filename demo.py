4a4475d634a986b3dbacaf8d9dd1e3125d490b8c
K G ANIRUDDHA <145186016+Kgaking@users.noreply.github.com>
2025-03-18 23:09:15 +0530

    Data Validation Done

--- demo.py
+++ demo.py
@@ -3,18 +3,20 @@
 # import sys
 
 
-# # logging.info("Welcome to Our Major-Project")
+# logging.info("Welcome to Our Major-Project")
 # logging.info("Zero Handling exception")
 
 
 # try:
-#     a = 2/0
+    # a = 2/0
 # except Exception as e:
 #     raise USvisaException(e, sys)
+
 
 # import os
 # mongo_db_url = os.getenv("MONGODB_URL")
 # print(mongo_db_url)
+
 
 from US_VISA.pipline.training_pipeline import TrainPipeline
 
